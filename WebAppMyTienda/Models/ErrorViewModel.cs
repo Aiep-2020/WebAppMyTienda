0be39934fcde8f406bfb337132d57605d849d3b6
jponce <contacto.jponce@gmail.com>
2020-09-24 01:01:41 -0300

    Estado inicial

---
+++ WebAppMyTienda/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAppMyTienda.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
