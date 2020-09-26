2e3351a22c05cb9313bb66549868cd110b23389a
jponce <contacto.jponce@gmail.com>
2020-09-26 02:49:40 -0300

    Ligas "save, Update"

--- WebAppMyTienda/Controllers/LigasController.cs
+++ WebAppMyTienda/Controllers/LigasController.cs
@@ -19,7 +19,57 @@ namespace WebAppMyTienda.Controllers
         {
             List<tbl_Ligas> ListLigas = await _context.Tbl_Ligas.ToListAsync();
             return View(ListLigas);
+        }
 
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            tbl_Ligas ListLigas = await _context.Tbl_Ligas.FindAsync(id);
+            return View(ListLigas);
+        }
+
+        [HttpPost]
+  
+        public async Task<IActionResult> Create([Bind("idliga,NombreLigas")] tbl_Ligas tbl_Ligas )
+        {
+
+            if (ModelState.IsValid) {
+                _context.Add(tbl_Ligas);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            
+            }
+
+            return View(tbl_Ligas);
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            tbl_Ligas ListLigas = await _context.Tbl_Ligas.FindAsync(id);
+            return View(ListLigas);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,NombreLigas")] tbl_Ligas tbl_Ligas)
+        {
+            if (ModelState.IsValid)
+            {
+
+                tbl_Ligas.idliga = id;
+                _context.Update(tbl_Ligas);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+
+            }
+            return View(tbl_Ligas);
         }
     }
 }
--- WebAppMyTienda/Models/Class/tbl_Ligas.cs
+++ WebAppMyTienda/Models/Class/tbl_Ligas.cs
@@ -5,7 +5,12 @@ namespace WebAppMyTienda.Models.Class
     public class tbl_Ligas
     {
         [Key]
-        public int idliga       { get; set; }
+        public int idliga { get; set; }
+
+        [Display(Name = "Nombre Liga")]
+        [Required(ErrorMessage = "Descripción es requerido")]
+        [StringLength(20, ErrorMessage = "Longitud máxima {1}")]
+        [MinLength(3, ErrorMessage = "Longitud minima {1}")]
         public string  NombreLigas  { get; set; }
     }
 }
