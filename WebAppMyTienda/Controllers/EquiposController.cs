2a2016ab10b9c09b3ff28e5ab8007a9269ab3d3a
jponce <contacto.jponce@gmail.com>
2020-10-21 09:14:15 -0300

    agregando DTo Para Equipos + Pagina  inicio Index.

---
+++ WebAppMyTienda/Controllers/EquiposController.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WebAppMyTienda.Models.Class;
+using WebAppMyTienda.Models.DTOs;
+
+namespace WebAppMyTienda.Controllers
+{
+    public class EquiposController : Controller
+    {
+
+        private readonly ApplicationDbContext _context;
+        private readonly IMapper mapper;
+
+        public EquiposController(ApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            this.mapper = mapper;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<tbl_Equipo> ListEquipos = await _context.tbl_Equipos.ToListAsync();
+            List<tbl_EquiposDTO> ListEquiposDto = mapper.Map<List<tbl_EquiposDTO>>(ListEquipos);
+
+            return View(ListEquiposDto);
+        }
+    }
+}
---
+++ WebAppMyTienda/Models/DTOs/tbl_EquiposCreacionDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppMyTienda.Models.DTOs
+{
+    public class tbl_EquiposCreacionDTO
+    {
+
+        [Display(Name = "Nombre Equipo")]
+        [Required(ErrorMessage = "Descripción es requerido")]
+        [StringLength(20, ErrorMessage = "Longitud máxima {1}")]
+        [MinLength(3, ErrorMessage = "Longitud minima {1}")]
+        public string NombreEquipo { get; set; }
+
+        public DateTime FechaFundc { get; set; }
+        public string Fotografia { get; set; }
+    }
+}
